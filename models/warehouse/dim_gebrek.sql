17bd58d0e34b10fcbe98d905c8b3b2542989c354
datavakwerk <datavakwerk@ruudjuffermans.nl>
2026-08-12 05:22:41 -0700

    fixed lint

--- models/warehouse/dim_gebrek.sql
+++ models/warehouse/dim_gebrek.sql
@@ -5,18 +5,27 @@ with gebreken as (
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['gebrek_identificatie']) }} as gebrek_key,
+    {{ dbt_utils.generate_surrogate_key(['gebrek_identificatie']) }}
+        as gebrek_key,
     gebrek_identificatie,
     gebrek_omschrijving,
     gebrek_artikel_nummer,
     gebrek_paragraaf_nummer,
     ingangsdatum_gebrek,
     einddatum_gebrek,
-    einddatum_gebrek is null as is_actueel
+    einddatum_gebrek is null
+        as is_actueel
 
 from gebreken
 
 union all
 
-select '-1', '(onbekend)', 'Onbekend gebrek', null, null, null, null, false
-gradientr
+select
+    '-1'              as gebrek_key,
+    '(onbekend)'      as gebrek_identificatie,
+    'Onbekend gebrek' as gebrek_omschrijving,
+    null              as gebrek_artikel_nummer,
+    null              as gebrek_paragraaf_nummer,
+    null              as ingangsdatum_gebrek,
+    null              as einddatum_gebrek,
+    false             as is_actueel
